30c42916f9a90652fcf8f094552fdbb7b52984e4
Felipe Sousa <felipesousa@brasilcap.com.br>
2018-05-09 16:13:42 -0300

    Input component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
-import { CapInputTextComponent } from './cap-inputText/cap-inputText.component';
+import { AppComponent } from "./app.component";
+import { CapInputTextComponent } from "./cap-inputText/cap-inputText.component";
 @NgModule({
-  declarations: [
-    AppComponent,
-    CapInputTextComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, CapInputTextComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/cap-inputText/cap-inputText.component.ts
+++ src/app/cap-inputText/cap-inputText.component.ts
@@ -1,4 +1,14 @@
-import { Component, Input, OnInit, ElementRef, AfterViewInit, ViewChild } from "@angular/core";
+import { Component, forwardRef, Input, OnInit, ElementRef, AfterViewInit, ViewChild, Output, EventEmitter } from "@angular/core";
+import { NG_VALUE_ACCESSOR, ControlValueAccessor } from "@angular/forms";
+
+const noop = () => {};
+
+export const CUSTOM_INPUT_CONTROL_VALUE_ACCESSOR: any = {
+  provide: NG_VALUE_ACCESSOR,
+  useExisting: forwardRef(() => CapInputTextComponent),
+  multi: true
+};
+
 declare var $: any;
 
 @Component({
@@ -6,10 +16,11 @@ declare var $: any;
   host: {
     class: "cap-inputText"
   },
+  providers: [CUSTOM_INPUT_CONTROL_VALUE_ACCESSOR],
   templateUrl: "./cap-inputText.component.html",
   styleUrls: ["./cap-inputText.component.css"]
 })
-export class CapInputTextComponent implements AfterViewInit {
+export class CapInputTextComponent implements AfterViewInit, ControlValueAccessor {
   @Input("id") id: string;
   @Input("label") label: string;
   @Input("placeholder") placeholder: string;
@@ -20,6 +31,39 @@ export class CapInputTextComponent implements AfterViewInit {
   @ViewChild("input") input;
 
   private $el: any;
+  private innerValue: any = "";
+
+  private onTouchedCallback: () => void = noop;
+  private onChangeCallback: (_: any) => void = noop;
+
+  get value(): any {
+    return this.innerValue;
+  }
+
+  set value(v: any) {
+    if (v !== this.innerValue) {
+      this.innerValue = v;
+      this.onChangeCallback(v);
+    }
+  }
+
+  onBlur() {
+    this.onTouchedCallback();
+  }
+
+  writeValue(value: any) {
+    if (value !== this.innerValue) {
+      this.innerValue = value;
+    }
+  }
+
+  registerOnChange(fn: any) {
+    this.onChangeCallback = fn;
+  }
+
+  registerOnTouched(fn: any) {
+    this.onTouchedCallback = fn;
+  }
 
   constructor(private el: ElementRef) {
     this.$el = $(el.nativeElement);
