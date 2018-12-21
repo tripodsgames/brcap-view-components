b3e19290f5e6187219ac08feab0b7143eaedee21
David Lerner <david.lerner@hotmail.com>
2018-12-21 16:04:29 -0200

    Tentando tirar observable

--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -1,4 +1,4 @@
-import { Observable } from "rxjs/Observable";
+// import { Observable } from "rxjs/Observable";
 import { Http, Response, Headers } from "@angular/http";
 import { Injectable } from "@angular/core";
 import * as Rx from "rxjs/Rx";
@@ -16,7 +16,7 @@ export class UsuarioService {
     this.headers.append("authorization", "testet");
   }
 
-  alterar(usuario: any, urlUsuarios):Observable<any> {
+  alterar(usuario: any, urlUsuarios) {
     const url = urlUsuarios + this.endpointUsuarios;
 
     return this._http.put(url, usuario, { headers: this.headers }).map(res => {
@@ -24,13 +24,13 @@ export class UsuarioService {
     });
   }
 
-  listarUsuarios(urlUsuarios):Observable<any> {
+  listarUsuarios(urlUsuarios) {
     const url = urlUsuarios + this.endpointUsuarios + "?plataforma=darwin";
 
     return this._http.get(url, { headers: this.headers }).map(res => res.json());
   }
 
-  permissionar(permissioes, login, sistema, urlUsuarios):Observable<any> {
+  permissionar(permissioes, login, sistema, urlUsuarios) {
     let url = urlUsuarios + this.endpointUsuarios;
     url += "/" + login;
     url += "%23" + sistema;
@@ -41,7 +41,7 @@ export class UsuarioService {
     return this._http.post(url, permissioes, { headers: this.headers }).map(res => res.json());
   }
 
-  buscaPermissoes(login, sistema, urlUsuarios):Observable<any> {
+  buscaPermissoes(login, sistema, urlUsuarios) {
     let url = urlUsuarios + this.endpointUsuarios;
     url += "/" + login;
     url += "%23" + sistema;
@@ -52,7 +52,7 @@ export class UsuarioService {
     return this._http.get(url, { headers: this.headers }).map(res => res.json());
   }
 
-  buscarEstadoPermissionamento(urlUsuarios, sistema, estadoPermissionamento):Observable<any> {
+  buscarEstadoPermissionamento(urlUsuarios, sistema, estadoPermissionamento) {
     let url = urlUsuarios + this.endpointUsuarios;
     url += "/plataformas/darwin/sistemas";
     url += "/" + sistema;
