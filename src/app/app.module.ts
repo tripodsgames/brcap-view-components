3b8e8af0c1cf3b63fdfd367029e200327b9982b3
David Lerner <david.lerner@hotmail.com>
2019-01-16 18:01:09 -0200

    versao

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,16 @@ import { CommonModule } from "@angular/common";
 import { FormsModule } from "@angular/forms";
 import { BRCapModule } from "./modules/brcap.module";
 
+import { CurrencyMaskModule } from "ng2-currency-mask";
+
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule, 
     CommonModule, 
     FormsModule, 
-    BRCapModule
+    BRCapModule,
+    CurrencyMaskModule
   ],
   bootstrap: [AppComponent]
 })
