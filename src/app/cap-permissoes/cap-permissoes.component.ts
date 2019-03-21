ac1d00b0f5022df20de677a78a9532afccb10ad8
David Lerner <david.lerner@hotmail.com>
2019-03-21 11:01:55 -0300

    atualizando pagination no cap-permissoes

--- src/app/cap-permissoes/cap-permissoes.component.ts
+++ src/app/cap-permissoes/cap-permissoes.component.ts
@@ -105,7 +105,9 @@ export class PermissoesComponent implements OnInit {
 
       this.usuariosPermissionados.forEach(element => {
         delete element.plataforma;
-        element.cpfMascarado = element.cpf.substring(0, 3).concat(".").concat(element.cpf.substring(3, 6)).concat(".").concat(element.cpf.substring(6, 9)).concat("-").concat(element.cpf.substring(9, 11));
+        if(element !== undefined){
+          element.cpfMascarado = element.cpf.substring(0, 3).concat(".").concat(element.cpf.substring(3, 6)).concat(".").concat(element.cpf.substring(6, 9)).concat("-").concat(element.cpf.substring(9, 11));
+        }
 
         if ((<any>Object).values(element).find((item) => item.toString().toUpperCase().indexOf(this.filtro.toUpperCase()) >= 0)) {
           this.listaFiltrado.push(element);
@@ -114,8 +116,9 @@ export class PermissoesComponent implements OnInit {
 
       this.usuariosNaoPermissionados.forEach(element => {
         delete element.plataforma;
+        if(element !== undefined){
         element.cpfMascarado = element.cpf.substring(0, 3).concat(".").concat(element.cpf.substring(3, 6)).concat(".").concat(element.cpf.substring(6, 9)).concat("-").concat(element.cpf.substring(9, 11));
-
+        }
         if ((<any>Object).values(element).find((item) => item.toString().toUpperCase().indexOf(this.filtro.toUpperCase()) >= 0)) {
           this.listaFiltrado.push(element);
         }
