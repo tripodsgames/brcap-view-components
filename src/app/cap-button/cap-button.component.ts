0da22f2155836536c511712a44395c95064d4f88
David Lerner <david.lerner@hotmail.com>
2019-07-26 18:04:23 -0300

    consertando font-size do cap-button

--- src/app/cap-button/cap-button.component.ts
+++ src/app/cap-button/cap-button.component.ts
@@ -26,8 +26,14 @@ export class CapButtonComponent implements OnInit {
       this.type = "button";
     }
 
-    if (this.styleClass && this.styleClass.indexOf('xs') !== -1) {
-      this.iconFontSize = 10;
+    if (this.styleClass && (this.styleClass.indexOf('xs') !== -1 || this.styleClass.indexOf('sm') !== -1)) {
+      this.iconFontSize = 13;
+    }
+    if (this.styleClass && this.styleClass.indexOf('icon-arrow') !== -1) {
+      this.iconFontSize = 16;
+    }
+    if (this.styleClass && (this.styleClass.indexOf('lg') !== -1 || this.styleClass.indexOf('fechar') !== -1)) {
+      this.iconFontSize = 24;
     }
   }
 }
