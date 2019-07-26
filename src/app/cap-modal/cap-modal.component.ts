4ba0f5389c27d6e3661cb17f8ba806552c17130c
David Lerner <david.lerner@hotmail.com>
2019-07-26 12:36:36 -0300

    consertos visuais do cap-modal

--- src/app/cap-modal/cap-modal.component.ts
+++ src/app/cap-modal/cap-modal.component.ts
@@ -10,7 +10,6 @@ export class CapModalComponent {
   public visibleAnimate = false;
 
   public show(): void {
-    console.log("oi")
     this.visible = true;
     setTimeout(() => (this.visibleAnimate = true), 100);
   }
