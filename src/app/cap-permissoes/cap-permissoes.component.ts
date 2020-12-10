051dc7146706cca547e90f95146ad311f1c3e587
Kelvin Gonçalves <kelvingoncalves@somosadd.com.br>
2020-12-10 17:47:39 -0300

    [Sprint 64 - LGPD] Atualização da montagem da tabela após filtragem de usuários

--- src/app/cap-permissoes/cap-permissoes.component.ts
+++ src/app/cap-permissoes/cap-permissoes.component.ts
@@ -107,9 +107,6 @@ export class PermissoesComponent implements OnInit {
 
       this.usuariosPermissionados.forEach(element => {
         delete element.plataforma;
-        if(element !== undefined){
-          element.cpfMascarado = element.cpf.substring(0, 3).concat(".").concat(element.cpf.substring(3, 6)).concat(".").concat(element.cpf.substring(6, 9)).concat("-").concat(element.cpf.substring(9, 11));
-        }
 
         if ((<any>Object).values(element).find((item) => item.toString().toUpperCase().indexOf(this.filtro.toUpperCase()) >= 0)) {
           this.listaFiltrado.push(element);
@@ -118,9 +115,7 @@ export class PermissoesComponent implements OnInit {
 
       this.usuariosNaoPermissionados.forEach(element => {
         delete element.plataforma;
-        if(element !== undefined){
-        element.cpfMascarado = element.cpf.substring(0, 3).concat(".").concat(element.cpf.substring(3, 6)).concat(".").concat(element.cpf.substring(6, 9)).concat("-").concat(element.cpf.substring(9, 11));
-        }
+
         if ((<any>Object).values(element).find((item) => item.toString().toUpperCase().indexOf(this.filtro.toUpperCase()) >= 0)) {
           this.listaFiltrado.push(element);
         }
