cfbeeb0969c2fbb04cb92486c7404020699c717c
tripodsgames <admin@lzptec.com>
2022-03-19 16:05:46 -0300

    Angular 11 - Quebrado.

--- src/app/cap-table-pagination/cap-table-pagination.component.spec.ts
+++ src/app/cap-table-pagination/cap-table-pagination.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { CapGridPaginationComponent } from './cap-grid-pagination.component';
 
@@ -6,7 +6,7 @@ describe('CapGridPaginationComponent', () => {
   let component: CapGridPaginationComponent;
   let fixture: ComponentFixture<CapGridPaginationComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ CapGridPaginationComponent ]
     })
